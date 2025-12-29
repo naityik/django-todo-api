42ad80f974d9972d510bcbac84261aeccc78ab67
Naityik Singh <142103737+naityik@users.noreply.github.com>
2025-12-29 13:52:53 +0530

    Updated urls.py

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -21,7 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')), # Link to your app URLs
-    path('polls/', include('polls.urls')), #previous Polls URLs
     path('login/', auth_views.LoginView.as_view(template_name='todo/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
